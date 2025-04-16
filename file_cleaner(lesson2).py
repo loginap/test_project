83f05654c277aec23c91eef17adb1aaa7c122a10
Loginap <kolomietsmihail2@gmail.com>
2025-04-16 19:32:53 +0300

    Версия для теста

---
+++ file_cleaner(lesson2).py
@@ -0,0 +1,7 @@
+from pathlib import Path
+import os
+(Path(Path.cwd()) / "test_folder").mkdir(parents=True, exist_ok=True)
+for i in range(10):
+    (Path(Path.cwd()) / "test_folder"/ f"txt_file{i+1}.txt").write_text(f"Привет номер {i+1}")
+    if i %2:
+        os.remove(str((Path(Path.cwd()) / "test_folder"/ f"txt_file{i+1}.txt")))
